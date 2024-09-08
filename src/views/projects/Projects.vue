bc05b2004069d1474c7db3ce1430935b9c56b52e
krazyjoy <huang.joy.0506@gmail.com>
2024-09-08 10:56:24 -0700

    add PRIDE to project

--- src/views/projects/Projects.vue
+++ src/views/projects/Projects.vue
@@ -1,47 +1,61 @@
 <template>
     <div class="section-container lh-sm">
-        <!-- <span class="title">Latest Projects</span> -->
-       
             <div v-for="(slide, index) in slides" :key="index">
                 <span class="title">{{slide.title}}</span>
-                <div class="slide-container p-3">
-                    <div class="slide-content" v-if="!isExpanded[slide.title]">
-                        <span class="slide-title mb-5 gs_reveal fromLeft" data-delay-sec="0.2" >{{slide.title}}</span>
-                        <span class="gs_reveal fromLeft" data-delay-sec="0.4">
-                            {{ slide.description }}
-                        </span>
-                        <span class="mt-3 gs_reveal fromLeft" data-delay-sec="0.6">
-                            <a :href="slide.github" class="icon_link">
-                                <img src="@/assets/github_icon-removebg-preview.png" class="icon"/>
-                            </a>
-                            <a role="button" :href="slide.graph" class="p-2 btn learn-more-btn gs_reveal fromLeft"  data-delay-sec="0.6" @click="clickRag(slide.title)">LEARN MORE</a>
-                        </span>
+                    <div class="slide-container p-3">
+                        <div class="slide-content"  v-if="!learnMoreFlag[slide.id] && !isExpanded[slide.id]">
+                            <span class="slide-title mb-5 gs_reveal fromLeft" data-delay-sec="0.2" >{{slide.title}}</span>
+                            <span class="gs_reveal fromLeft" data-delay-sec="0.4">
+                                {{ slide.description }}
+                            </span>
+                            <span class="mt-3 gs_reveal fromLeft" data-delay-sec="0.6">
+                                <a :href="slide.github" class="icon_link">
+                                    <img src="@/assets/github_icon-removebg-preview.png" class="icon"/>
+                                </a>
+                                <button type="button" class="p-2 btn learn-more-btn gs_reveal fromLeft"  data-delay-sec="0.6" @click="learnMore(slide.id)">LEARN MORE</button>
+                            </span>
+                        </div>
+                        <div class="slide-image" v-if="!isExpanded[slide.id] && !learnMoreFlag[slide.id]">
+                            <img :src="slide.image" :alt="slide.image" class="gs_reveal fromRight" data-delay-sec="0.6" @click="clickImg(slide.id)" style="max-width: 40vw; max-height: 50vh; object-fit: cover; border-radius: 10px; margin-right: 10px;"/>
+                        </div>
+                        <div class="child-component component">
+                            <MindMap @close_mindmap="toggleMindMap" v-if="learnMoreFlag['RAG'] && slide.id==='RAG'"/>
+                        </div>
+                        <div class="child-component component">
+                            <Pride @close_pride="togglePride" v-if="learnMoreFlag['PRIDE']  && slide.id==='PRIDE'"/>
+                        </div>
+                        <div class="child-component component">
+                            <RagComponent  v-if="isExpanded['RAG'] && slide.id==='RAG'" :expand="isExpanded['RAG']" @close_expand="toggleExpand('RAG')" />
+                        </div>
+                        <div class="child-component component">
+                            <PrideComponent  v-if="isExpanded['PRIDE'] && slide.id==='PRIDE'" :expand="isExpanded['PRIDE']" @close_expand="toggleExpand('PRIDE')" />
+                        </div>
                     </div>
                     
-                    <img :src="slide.image" :alt="slide.image" class="slide-image gs_reveal fromRight" data-delay-sec="0.6" @click="hoverImg(slide.title)" v-if="!isExpanded[slide.title] || currSlide != slide.title"/>
-                    
-                    <RagComponent class="child-component component" v-if="isExpanded['Internal AI Mentor'] && slide.title ==='Internal AI Mentor'" :expand="isExpanded['Internal AI Mentor']" @close_expand="toggleExpand('Internal AI Mentor')" />
-                    
-                </div>
+<!--                     
+                </div> -->
             </div>
-            <MindMap v-if="isRag"/>
             
     </div>
         
 </template>
 <script setup>
-import {onBeforeMount, onMounted, ref} from 'vue';
+import {onMounted, ref} from 'vue';
 import kgilImage from '@/assets/projects/rag_profile.png';
 import djangoSSO from '@/assets/projects/django-third-party-login.png';
 import iaasImage from '@/assets/projects/iaas-elastic-app.jpg';
 import aiCupImage from '@/assets/projects/voice-disease-analysis.png';
+import prideImage from '@/assets/projects/learning_diagram_v3.png';
 import gsap from 'gsap';
 import MindMap from '../langchain/MindMap.vue';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import RagComponent from '@/components/project/RagComponent.vue';
+import PrideComponent from '@/components/project/Pride.vue'
+import Pride from '@/views/pride/pride.vue';
 
 const slides = [
     {
+        id: 'RAG',
         title: 'Internal AI Mentor', 
         image: kgilImage,
         description: 'A comprehensive course training management platform meticulously designed to empower a company’s workforce with the specialized knowledge and skills essential for each department’s success. This website serves as an invaluable resource, offering tailored training programs that ensure employees are well-equipped to excel in their respective roles, fostering both individual growth and overall organizational excellence.',
@@ -49,6 +63,15 @@ const slides = [
         graph: "/vue-githubio/rag"
     },
     {
+        id: 'PRIDE',
+        title: 'PRIDE-multimodal early depression detection system',
+        image: prideImage,
+        description: 'A cloud eployed system running on edge devices for inferencing user depression level',
+        github: 'https://github.com/krazyjoy/PRIDE-Depression-Detection-Transformer-Based-System',
+        graph: '#'
+    },
+    {
+        id: 'AWS',
         title: 'IaaS Elastic Face Recognition App', 
         image: iaasImage,
         description: 'An advanced EC2 web tier eagerly awaits users to upload images, seamlessly queuing them in an SQS Queue for processing. Leveraging the robust capabilities of the elastic image recognition FaceNet model, the system meticulously interprets each image, delivering insightful expression analysis back to the application tier, ensuring a responsive and intelligent user experience.',
@@ -56,6 +79,7 @@ const slides = [
         graph: "#"
     },
     {
+        id: 'Django',
         title: 'Django SSO App',
         image: djangoSSO,
         description: 'A login web application supports Google/Facebook third party login method using Django backend and React framework.',
@@ -63,6 +87,7 @@ const slides = [
         graph: "#"
     },
     {
+        id: 'AICUP',
         title: 'Voice Disease Analysis',
         image: aiCupImage,
         description: '',
@@ -71,15 +96,22 @@ const slides = [
     }
 ];
 
-const isScrolling = ref(false);
-const currSlide = ref('');
-const isRag = ref(false);
-const isExpanded = ref({});
 
+const currSlide = ref('');
+
+const isExpanded = ref({});
+const learnMoreFlag = ref({});
 
 
 const toggleExpand = (title) => {
     isExpanded.value[title] = !isExpanded.value[title];
+
+}
+const toggleMindMap = () => {
+    learnMoreFlag.value['RAG'] = false;
+}
+const togglePride = () => {
+    learnMoreFlag.value['PRIDE'] = false;
 }
 /* gsap */
 gsap.registerPlugin(ScrollTrigger);
@@ -108,10 +140,13 @@ onMounted(()=>{
         })
     })
     slides.forEach((slide)=>{
-        isExpanded.value[slide.title] = false;
+        isExpanded.value[slide.id] = false;
         
     })
-    console.log("isExpanded: ", isExpanded.value)
+    console.log("onMounted Expand status: ", isExpanded.value);
+    slides.forEach((slide)=>{
+        learnMoreFlag.value[slide.id] = false;
+    })
 
 });
 
@@ -151,13 +186,6 @@ const animateFrom = (elem, direction=1)=>{
         }
     )
 }
-const imagePaths = {
-    'Internal AI Mentor': kgilImage,
-    'IaaS Elastic Face Recognition App': iaasImage,
-    'Django SSO App': djangoSSO,
-    'Voice Disease Analysis': aiCupImage
-};
-
 
 
 
@@ -165,14 +193,14 @@ const imgFlipped = ref({
     'Internal AI Mentor': false,
     'IaaS Elastic Face Recognition App': false,
     'Django SSO App': false,
-    'Voice Disease Analysis': false
+    'Voice Disease Analysis': false,
+    'PRIDE-multimodal early depression detection system': false,
 });
-const hoverImg = (image_name) => {
+const clickImg = (image_name) => {
     if(imgFlipped.value.hasOwnProperty(image_name)){
         console.log("image is hovered")
         imgFlipped.value[image_name] = true;
         currSlide.value = image_name;
-       
         isExpanded.value[image_name] = true;
 
     }
@@ -181,6 +209,13 @@ const hoverImg = (image_name) => {
     }
 }
 
+
+const learnMore = (title) => {
+    console.log("learn more button");
+    learnMoreFlag.value[title] = !learnMoreFlag.value[title];
+    console.log(title);
+    console.log(learnMoreFlag.value[title]);
+}
 
 
 </script>
@@ -212,25 +247,25 @@ const hoverImg = (image_name) => {
 
 .slide-container{
     display: flex;
+    width: 80%;
     flex-direction: row;
     flex-wrap: nowrap;
     align-items: center;
     overflow: hidden;
     height: 80vh;
-    margin-right: 20px;
+    margin: 0 auto;
     box-sizing: border-box;
     position: relative;
-    margin-left: 20px;
+
     justify-content: center;
     margin-top: 20px;
 }
 .slide-content{
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: space-around;
     font-family: "Popins", sans-serif;
-    width: 40%;
-    z-index: 1;
+    flex: 1;
     box-sizing: border-box;
     margin-right: 10px;
     padding-right: 20px;
@@ -240,12 +275,8 @@ const hoverImg = (image_name) => {
 }
 
 .slide-image{
-
-    width: 40%;
-    max-height: 50vh;
-    object-fit: cover;
-    border-radius: 10px;
-    margin-right: 10px;
+    display: flex;
+    flex: 1;
     cursor: pointer;
     justify-content: center;
     
